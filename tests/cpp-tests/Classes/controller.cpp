202b2638553765aadda5770e5daf2cba61e7f20e
boyu0 <yubo@cocos2d-x.org>
2014-06-17 18:30:56 +0800

    performance test for tmx

--- tests/cpp-tests/Classes/controller.cpp
+++ tests/cpp-tests/Classes/controller.cpp
@@ -34,6 +34,7 @@ Controller g_aTestNames[] = {
     // TESTS MUST BE ORDERED ALPHABETICALLY
     //     violators will be prosecuted
     //
+	{ "Node: TileMapNew", [](){return new TileMapTestSceneNew(); } },
 	{ "ActionManager", [](){return new ActionManagerTestScene(); } },
 	{ "Actions - Basic", [](){ return new ActionsTestScene(); } },
 	{ "Actions - Ease", [](){return new ActionsEaseTestScene();} },
